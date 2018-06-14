0415e1c94103bb63688c8eacd48c71f99ef2c367
Dennis Playdon <37030744+DennisPlaydon@users.noreply.github.com>
2018-06-15 11:11:43 +1200

    Initial commit

---
+++ easySearch.py
@@ -0,0 +1,30 @@
+#! python3
+# easySearch.py - Launches a search in the browser using an address from the
+# command line
+
+import webbrowser, sys, pyperclip
+
+website = 0
+def type(x):
+    x = x.lower()
+    return {
+        'go': 1,
+        'yt': 2,
+        'weather': 3,
+        'map': 4
+    }.get(x, 0)
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    website = type(sys.argv[1])
+    address = ' '.join(sys.argv[2:])
+
+if website != 0:
+    if website == 1:
+        webbrowser.open("https://www.google.com.tr/search?q={}".format(address))
+    elif website == 2:
+        webbrowser.open("https://www.youtube.com/results?search_query={}".format(address))
+    elif website == 3:
+        webbrowser.open("https://weather.com")
+    elif website == 4:
+        webbrowser.open('https://www.google.com/maps/place/' + address)
